d948434d786ea2e558f40d1fbb54cacafa65eddb
qvalc <qvalc.be@gmail.com>
2026-06-18 21:10:59 +0200

    Lancement du site

---
+++ script.js
@@ -0,0 +1,17 @@
+const menuBtn = document.getElementById("menuBtn");
+const nav = document.getElementById("nav");
+
+menuBtn.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
+
+document.querySelectorAll(".project-card").forEach((card) => {
+  const mainImage = card.querySelector(".project-card > img");
+  const thumbs = card.querySelectorAll(".project-thumbs img");
+
+  thumbs.forEach((thumb) => {
+    thumb.addEventListener("click", () => {
+      mainImage.src = thumb.src;
+    });
+  });
+});
